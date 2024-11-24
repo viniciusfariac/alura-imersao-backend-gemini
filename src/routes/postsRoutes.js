b3015dd18c05a0cfdd3f9e46175524419ffeb3f2
viniciusfariac <vfariacerqueira@gmail.com>
2024-11-24 17:20:51 -0300

    add = add project

---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,31 @@
+import express from "express"
+import multer from "multer";
+import cors from "cors"
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postControllers.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSucessStatus: 200
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
+
+const routes = (app) => {
+    app.use(express.json()) 
+    app.use(cors(corsOptions))
+    app.get("/posts", listarPosts)
+    app.post("/posts", postarNovoPost)
+    app.post("/upload", upload.single("Imagem"), uploadImagem)
+    app.put("/upload/:id", atualizarNovoPost)
+}
+
+export default routes;
